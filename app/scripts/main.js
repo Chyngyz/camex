8f5075a19016cf7bb35dec35352abc18a8b2ae06
Chyngyz <chyngyz@187.corund.local>
2015-11-13 17:03:52 +0600

    Header done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,78 @@
-console.log('\'Allo \'Allo!');
+(function($){
+	$(document).ready(function() {
+
+		
+
+ 		// Home page hero slider
+	  	$("#hero-slider").owlCarousel({
+			loop : true,
+			autoplay : true,
+			autoplayHoverPause : true,
+			fluidSpeed : 600,
+			autoplaySpeed : 600,
+			dotsSpeed : 600,
+			dragEndSpeed : 60,
+		    slideSpeed : 300,
+		    paginationSpeed : 400,
+		    singleItem:true,
+		    autoPlay : true,
+    		stopOnHover : false,
+    		autoHeight : true,
+    		transitionStyle: "fade"
+		 
+		    // "singleItem:true" is a shortcut for:
+		    // items : 1, 
+		    // itemsDesktop : false,
+		    // itemsDesktopSmall : false,
+		    // itemsTablet: false,
+		    // itemsMobile : false
+	 
+	  	});
+
+
+		// Go top!
+	    $('#goto-top').on('click', function () {
+	        $('html, body').animate({scrollTop: 0}, 'slow');
+	    });
+
+	    // Show fixed footer
+	    $(window).on("scroll", function () {
+	        if ($(window).scrollTop() > 200){
+	            $('#fixed-contacts').addClass('shown');
+	        }
+	        else if($(window).scrollTop() < 200) {
+	            $('#fixed-contacts').removeClass('shown');
+	        }
+	    });
+
+	    
+
+	});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
